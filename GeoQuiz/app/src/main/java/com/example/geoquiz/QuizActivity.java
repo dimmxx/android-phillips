2fbd8895a5fb3be999dc10e81b3422083e2d01ae
dimmxx <dimmxx@gmail.com>
2020-06-04 16:14:54 +0300

    land

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.example.geoquiz;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -8,17 +9,23 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
 
     //app/build/intermediates/runtime_symbol_list/<insert build type here>/R.txt
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPreviousButton;
     private ImageButton mPreviousImageButton;
+
+    private int mCurrentIndex;
 
     private TextView mQuestionTextView;
     private TextView mProgressTextView;
@@ -32,13 +39,23 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true),
     };
 
-    private int mCurrentIndex = 0;
+
+
+
+
+
     private int mCorrectAnswer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+    if(savedInstanceState != null){
+        mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    }
+
 
         View trueButtonView = findViewById(R.id.true_button);
         View falseButtonView = findViewById(R.id.false_button);
@@ -132,4 +149,40 @@ public class QuizActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "inSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
 }
